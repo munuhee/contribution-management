cc386d604bb1ca67a26689e88147043f2bcea6b1
munuhee <murichustephen@gmail.com>
2024-07-22 16:51:19 +0300

    Modify to include messages

--- cases/views.py
+++ cases/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Case
 from .forms import CaseForm
-# from notifications.sms_utils import send_sms
-
 
 # List all cases
 @login_required
@@ -11,13 +10,11 @@ def list_cases(request):
     cases = Case.objects.all()
     return render(request, 'cases/cases_list.html', {'cases': cases})
 
-
 # Detail view for a specific case
 @login_required
 def detail_case(request, case_id):
     case = get_object_or_404(Case, id=case_id)
     return render(request, 'cases/case_detail.html', {'case': case})
-
 
 # Add a new case
 @login_required
@@ -26,11 +23,13 @@ def add_case(request):
         form = CaseForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Case added successfully!')
             return redirect('list_cases')
+        else:
+            messages.error(request, 'Please correct the errors below.')
     else:
         form = CaseForm()
     return render(request, 'cases/cases_form.html', {'form': form})
-
 
 # Update a case
 @login_required
@@ -40,15 +39,18 @@ def update_case(request, case_id):
         form = CaseForm(request.POST, instance=case)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Case updated successfully!')
             return redirect('list_cases')
+        else:
+            messages.error(request, 'Please correct the errors below.')
     else:
         form = CaseForm(instance=case)
     return render(request, 'cases/cases_form.html', {'form': form})
-
 
 # Delete a case
 @login_required
 def delete_case(request, case_id):
     case = get_object_or_404(Case, id=case_id)
     case.delete()
+    messages.success(request, 'Case deleted successfully!')
     return redirect('list_cases')
